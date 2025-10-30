956791f6cab4f89b4198e4185e0deb67bcd9cc55
daeunxxx <daeunxx@gmail.com>
2025-10-30 16:08:19 +0900

    forEach 연습

--- 1028_practice.js
+++ 1028_practice.js
@@ -1,3 +1,24 @@
 const fruits = [ 'apple', 'banana', 'cherry' ];
 
+fruits.forEach((fruit, idx) => {
+    console.log(`${idx + 1}. ${fruit}`);
+});
 
+
+
+const animals = ['dog', 'cat', 'rabbit'];
+
+animals.forEach((animal, i) => {
+    console.log(`${i + 1}. ${animal}`);
+});
+
+
+
+const numbers = [1, 2, 3];
+numbers.forEach(n => console.log(n * 2));
+
+
+const texts = ["김", "다", "은"]
+texts.forEach((text, idx) => {
+    console.log(`${idx + 1}번째 : ${text}`);
+});
